0723d609199b9276d924d17478faefd28fa49b59
Zoren Villareal <zoren.villareal@gmail.com>
2023-06-04 13:29:39 -0700

    Got columns and rows aside properly layed out

--- dealOrNoDeal.js
+++ dealOrNoDeal.js
@@ -19,7 +19,7 @@ console.log(div.length)
 // add all remaining unopened cases banker offer
 // loop over cases and open
 // if the price on board delete
-let prize = [0.01,1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000,2000000]
+let prize = [1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000,2000000]
 console.log(prize.length)
 //github copilot vscode hotkeys vscode
 let cases = {}
