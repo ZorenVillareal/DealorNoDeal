da4305e0a2baed9bea54770e55ed2a37405029f4
Zoren Villareal <zoren.villareal@gmail.com>
2023-06-05 23:50:33 -0700

    was able to fill in prizes div using JS

--- dealOrNoDeal.js
+++ dealOrNoDeal.js
@@ -1,34 +1,48 @@
-//array of prices
-// array of cases
-
-//set constants:
-// let limit;
-// let opened;
-// let prizes = []
-// let keepCaseValue;
-// let otherCase;
-let opened = []
-
-let keepCaseValue =[];
-let openedCaseValue =[];
-let price = 0;
+let prizeCount = 0
+let casesCount = 26
+let numberOfRounds = 6
 // let cases =  {A:price , B: price, C: price, D: price, E: price, F: price, G: price, H: price, I: price, J: price, K: price, L: price, M: price, N: price, O: price, P: price, Q: price, R: price, S: price, T: price, U: price, V: price, W: price, X: price, Y: price, Z: price}
 // console.log(cases.length)
-let div = []
-console.log(div.length)
+
 // add all remaining unopened cases banker offer
 // loop over cases and open
 // if the price on board delete
-let prize = [1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000,2000000]
-console.log(prize.length)
-//github copilot vscode hotkeys vscode
+let prizes = [1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000,2000000]
+// console.log(prize.length)
 let cases = {}
-let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-function fillInCase (prizes,cases,letters){
-    for (let i = 0; i < prizes.length; i++){
-        cases[letters[i]] = prizes[i]
+// let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+// function fillInCase (prizes,cases,letters){
+//     // let randomized =;
+//     for (let i = 0; i < prizes.length; i++){
+//         cases[letters[i]] = prizes[i]
+//     }
+//     // console.log(cases)
+//     return cases
+// }
+// fillInCase(prizes,cases,letters)
+
+// round one take 6 cases off
+// round two take 5 cases off
+// round three take 4 cases off
+// round four take 3 cases off
+// round five take 2 cases off
+// round six take 1 case off
+// round seven take 1 case off
+// round eight take 1 case off
+// round nine take 1 case off
+
+let smallPrizeList = document.querySelector('#smallPrizeList').children
+// console.log(smallPrizeList)
+function fillInPrizesLeft (smallPrizeList,prizes){
+    for (let i = 0; i < smallPrizeList.length; i++){
+            smallPrizeList[i].innerHTML = prizes[i]
     }
-    console.log(cases)
-    return cases
 }
-fillInCase(prize,cases,letters)
+fillInPrizesLeft(smallPrizeList,prizes);
+let bigPrizeList = document.querySelector('#bigPrizeList').children
+function fillInPrizesRight(bigPrizeList,prizes){
+    for (let i = 0; i < bigPrizeList.length; i++){
+        bigPrizeList[i].innerHTML = prizes[i+13]
+    }
+}
+fillInPrizesRight(bigPrizeList,prizes)
